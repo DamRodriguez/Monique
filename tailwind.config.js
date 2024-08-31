9ffe31a8a40ea28b9c55b08af72c44203f69fd26
Damian Rodriguez <141194257+DamRodriguez@users.noreply.github.com>
2024-08-31 00:18:05 -0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     content: [
-      './public/**/*.{html,js}',
-      './src/**/*.{html,js}',
-      './node_modules/flowbite/**/*.js',
+      './*.{html,js}',  // Cambiado para buscar en la raíz
+      './src/**/*.{html,js}',  // Asegúrate de que esta carpeta existe y contiene archivos HTML/JS
+      './node_modules/flowbite/**/*.js',  // Para Flowbite
     ],
     theme: {
       extend: {
@@ -14,5 +14,7 @@ module.exports = {
     plugins: [
       require('@tailwindcss/forms'),  // Si usas algún plugin como Tailwind Forms
       require('daisyui'),  // Ejemplo de plugin adicional
+      require('flowbite/plugin'),  // Para Flowbite
     ],
-}
+  }
+  
