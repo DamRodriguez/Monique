f196e522c103a100ded9bcbaa433d3fda3b47543
Damian Rodriguez <141194257+DamRodriguez@users.noreply.github.com>
2024-09-03 20:44:08 -0300

    cambio total

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,74 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navbarElement = document.getElementById('navbar');
-  
-    if (navbarElement) {
-      navbarElement.innerHTML = `
-        <div class="navbar bg-opacity-90 navFondo fixed top-0 left-0 w-full z-50">
-          <div class="navbar-start">
-            <div class="dropdown">
-              <div tabindex="0" role="button" class="btn btn-ghost btn-circle hover:bg-opacity-20 hover:bg-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 text-gray-200"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h7" />
-                </svg>
-              </div>
-              <ul
-                tabindex="0"
-                class="menu menu-lg dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow text-gray-50 fondoDropdown">
-                <li><a class="menu-item hover:bg-opacity-20 hover:bg-black">Inicio</a></li>
-                <li><a class="menu-item hover:bg-opacity-20 hover:bg-black">Productos</a></li>
-                <li><a class="menu-item hover:bg-opacity-20 hover:bg-black">Información</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="navbar-center">
-            <a class="btn btn-ghost font-serif hover:bg-transparent">
-              <img class="filter invert brightness-0 w-40" src="/public/assets/images/monique-logo.png" alt="Logo-de-Monique">
-            </a>
-          </div>
-          <div class="navbar-end">
-            <button class="btn btn-ghost btn-circle hover:bg-opacity-20 hover:bg-black">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-gray-200"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </button>
-            <button class="btn btn-ghost btn-circle hover:bg-opacity-20 hover:bg-black">
-              <div class="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 text-gray-200"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                </svg>
-                <span class="badge badge-xs badge-primary indicator-item"></span>
-              </div>
-            </button>
-          </div>
-        </div>
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarElement = document.getElementById("navbar");
+
+  if (navbarElement) {
+    navbarElement.innerHTML = `
+        
+
+<div class="border-gray-200 dark:bg-gray-800 dark:border-gray-700 fixed w-full z-20 top-0 start-0 bg-transparent">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="public/assets/images/monique-logo.png" class="h-8" alt="Monique Logo"/>
+    </a>
+    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-purple-300 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+    <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        <li>
+            <a href="#" class="block py-2 px-3 text-base xl:text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-300 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Inicio</a>
+        </li>
+        <li>
+            <a href="#" class="block py-2 px-3 text-base xl:text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-300 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Productos</a>
+        </li>
+        <li>
+            <a href="#" class="block py-2 px-3 text-base xl:text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-300 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contacto</a>
+        </li>
+    </ul>
+
+    </div>
+  </div>
+</div>
+
       `;
-    }
-  });
-  
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-    content: [
-      './*.{html,js}',  // Cambiado para buscar en la raíz
-      './src/**/*.{html,js}',  // Asegúrate de que esta carpeta existe y contiene archivos HTML/JS
-      './node_modules/flowbite/**/*.js',  // Para Flowbite
-    ],
-    theme: {
-      extend: {
-        colors: {
-          'custom-blue': '#1fb6ff',
-        },
-      },
-    },
-    plugins: [
-      require('@tailwindcss/forms'),  // Si usas algún plugin como Tailwind Forms
-      require('daisyui'),  // Ejemplo de plugin adicional
-      require('flowbite/plugin'),  // Para Flowbite
-    ],
-  }
-  
+  content: [
+    './*.{html,js}',  
+    './src/**/*.{html,js}',  
+    './node_modules/flowbite/**/*.js',  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),  
+    require('daisyui'),  
+    require('flowbite/plugin'),
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+}
