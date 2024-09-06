484813746735733f8e40e69d99778d8996d4f135
Damian Rodriguez <141194257+DamRodriguez@users.noreply.github.com>
2024-09-06 17:37:29 -0300

    nav activo

---
+++ src/js/navActivo.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname; // Obtén la URL actual
+    const links = document.querySelectorAll(".navLink"); // Selecciona todos los enlaces con la clase nav-link
+  
+    links.forEach(link => {
+      const linkPath = new URL(link.href).pathname; // Obtén el path del href del enlace
+      if (currentPath === linkPath) {
+        link.classList.add("activo"); // Agrega la clase activa al enlace que coincide
+      }
+    });
+  });
