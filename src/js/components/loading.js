0c880127f61a0faaa8b87df8b0eaf2f7c5828b94
Damian Rodriguez <141194257+DamRodriguez@users.noreply.github.com>
2024-09-12 16:42:30 -0300

    .

--- src/js/components/loading.js
+++ src/js/components/loading.js
@@ -1,9 +1,17 @@
-// src/js/componentes/loading.js
-
 export function toggleLoadingSpinner(visible) {
-    const loadingSpinner = document.getElementById('loading-spinner'); // Asegúrate de que este ID sea correcto
+    const loadingSpinner = document.getElementById('loading-spinner'); 
+    const prevPageButton = document.getElementById('prev-page');
+    const nextPageButton = document.getElementById('next-page');
+    
     if (loadingSpinner) {
+        // Mostrar u ocultar el spinner
         loadingSpinner.style.display = visible ? 'flex' : 'none';
+        
+        // Mostrar u ocultar las flechas de paginación cuando el spinner esté visible
+        if (prevPageButton && nextPageButton) {
+            prevPageButton.style.display = visible ? 'none' : 'inline-block';
+            nextPageButton.style.display = visible ? 'none' : 'inline-block';
+        }
     } else {
         console.error('No se encontró el elemento con el ID "loading-spinner".');
     }
@@ -11,6 +19,7 @@ export function toggleLoadingSpinner(visible) {
 
 export function loadJSONData() {
     toggleLoadingSpinner(true);
+
     return fetch('../../public/data/productos.json')
         .then(response => response.json())
         .catch(error => {
