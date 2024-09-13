8c880895094e3cce17df64c9a2ce89dffb7cbca4
Damian Rodriguez <141194257+DamRodriguez@users.noreply.github.com>
2024-09-12 22:41:29 -0300

    .

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -11,8 +11,6 @@ export function updateGallery() {
     const gallery = document.getElementById('gallery');
     const noProductsMessage = document.getElementById('no-products-message');
     const paginationButtonsContainer = document.getElementById('pagination');
-    const prevPageButton = document.getElementById('prev-page');
-    const nextPageButton = document.getElementById('next-page');
 
     // Mostrar u ocultar mensaje y botones de paginación
     if (filteredItems.length === 0) {
@@ -30,43 +28,33 @@ export function updateGallery() {
             const div = document.createElement('div');
             div.className = 'grid overflow-hidden product-item rounded-sm shadow hover:scale-105 transform transition-all ease-in rounded-tl-3xl rounded-br-3xl itemFondo shadowColor2 sombraInset2 itemFondo hover:border-2 hover:border-white';
             div.style.cursor = 'pointer';
-            const modalId = `modal-${startIndex + index}`; // ID único para cada modal
+            const modalId = `modal-${startIndex + index}`; // Ajuste para el ID del modal
 
             div.innerHTML = `
                 <img src="${product.image}" class="opacity-0 shadowColor2 shadow-lg h-auto max-w-full rounded-tl-3xl rounded-br-3xl rounded-sm" alt="${product.name}">
                 <h2 class="text-end p-2 pr-4 md:pr-6 xl:pr-8 relative backdrop-blur-lg bg-white bg-opacity-25 sombraTop w-full h-full xl:text-2xl md:text-2x1 text-lg font-semibold mt-5 text-shadow-md text-gray-50">${product.name}</h2>
+                
+                <dialog id="${modalId}" class="modal cursor-auto backdrop-blur-sm shadow-lg">
+                    <div class="modal-box w-11/12 max-w-5xl h-2/3 grid"> 
+                        <h2 class="text-3xl md:text-4xl xl:text-6xl font-bold">${product.name}</h2>
+                        <p class="py-4">${product.description}</p>
+                        <div class="modal-action items-end">
+                            <form method="dialog">
+                                <button class="btn">Close</button>
+                            </form>
+                        </div>
+                    </div>
+                </dialog>
             `;
 
             gallery.appendChild(div);
 
-            // Evento para crear y mostrar modal cuando el producto es clicado
             div.addEventListener('click', () => {
-                let modal = document.getElementById(modalId);
-                
-                // Crear modal si no existe
-                if (!modal) {
-                    modal = document.createElement('dialog');
-                    modal.id = modalId;
-                    modal.className = 'modal cursor-auto backdrop-blur-sm shadow-lg';
-                    modal.innerHTML = `
-                        <div class="modal-box w-11/12 max-w-5xl h-2/3 grid">
-                            <h2 class="text-3xl md:text-4xl xl:text-6xl font-bold">${product.name}</h2>
-                            <p class="py-4">${product.description}</p>
-                            <div class="modal-action items-end">
-                                <form method="dialog">
-                                    <button class="btn">Close</button>
-                                </form>
-                            </div>
-                        </div>
-                    `;
-                    document.body.appendChild(modal); // Añadir el modal al body solo cuando es necesario
-                }
-
-                modal.showModal(); // Mostrar el modal
+                document.getElementById(modalId).showModal();
             });
         });
 
-        renderPagination(filteredItems.length);
+        renderPagination(filteredItems.length);  // Aquí llamamos a renderPagination
     }
 
     window.scrollTo(0, 0);
